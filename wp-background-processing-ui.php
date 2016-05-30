c4c2a8c8e6441c6d15b9ef302a7d333c96902ac5
Felix Arntz <felix-arntz@t-online.de>
2016-05-30 20:46:56 +0200

    author URL change

--- wp-background-processing-ui.php
+++ wp-background-processing-ui.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: WP Background Processing UI
-Plugin URI: https://github.com/felixarntz/wp-background-processing-ui
+Plugin URI:  https://github.com/felixarntz/wp-background-processing-ui
 Description: This library contains classes to perform background processes in WordPress including a visual UI. Based on A5hleyRich/wp-background-processing.
-Version: 1.0.0
-Author: Felix Arntz
-Author URI: http://leaves-and-love.net
+Version:     1.0.0
+Author:      Felix Arntz
+Author URI:  https://leaves-and-love.net
 */
 
 require_once plugin_dir_path( __FILE__ ) . 'vendor/A5hleyRich/wp-background-processing/wp-background-processing.php';
